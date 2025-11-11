f27e15c9ffb1ee3e18bf8bfcf9a7df5a4622ce76
renee <renee@vanderwalt.eu>
2025-11-11 15:41:11 +0100

    assets

---
+++ src/assets/cookieconsent-config.js
@@ -0,0 +1,52 @@
+import 'https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@3.1.0/dist/cookieconsent.umd.js';
+
+CookieConsent.run({
+
+    categories: {
+        necessary: {
+            enabled: true,  // this category is enabled by default
+            readOnly: true  // this category cannot be disabled
+        },
+        analytics: {}
+    },
+
+    language: {
+        default: 'en',
+        translations: {
+            en: {
+                consentModal: {
+                    title: 'I use cookies',
+                    description: 'Cookie modal description',
+                    acceptAllBtn: 'Accept all',
+                    acceptNecessaryBtn: 'Reject all',
+                    showPreferencesBtn: 'Manage your preferences'
+                },
+                preferencesModal: {
+                    title: 'Manage cookie preferences',
+                    acceptAllBtn: 'Accept all',
+                    acceptNecessaryBtn: 'Reject all',
+                    savePreferencesBtn: 'Accept current selection',
+                    closeIconLabel: 'Close modal',
+                    sections: [
+                        {
+                            title: 'Strictly Necessary Cookies',
+                            description: 'These cookies are essential for the proper functioning of the website and cannot be disabled.',
+
+                            //this field will generate a toggle linked to the 'necessary' category
+                            linkedCategory: 'necessary'
+                        },
+                        {
+                            title: 'Performance and Analytics',
+                            description: 'These cookies collect information about how you use my website. All of the data is anonymized and cannot be used to identify you.',
+                            linkedCategory: 'analytics'
+                        },
+                        {
+                            title: 'More information',
+                            description: 'For any queries in relation to my policy on cookies and your choices, please <a href="mailto:renee@vanderwalt.eu">contact me</a>.'
+                        }
+                    ]
+                }
+            }
+        }
+    }
+});
